f1157ac51f8ac07cba556e7cc016ae951580a79a
TaylanCann <taylancanh@gmail.com>
2022-01-18 17:42:37 +0300

    Add all of funcks

--- funcsOfStringValues/Program.cs
+++ funcsOfStringValues/Program.cs
@@ -6,10 +6,51 @@ namespace funcsOfStringValues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bir kelime giriniz lütfen : ");
+            Console.WriteLine("Bir cümle giriniz lütfen : ");
 
             string denemeDegiskeni;
             denemeDegiskeni = Console.ReadLine();
+            //Console.WriteLine(denemeDegiskeni.AsMemory);
+            denemeDegiskeni.AsMemory();
+            denemeDegiskeni.AsSpan();
+            denemeDegiskeni.Clone();
+            //denemeDegiskeni.CompareTo();
+            //denemeDegiskeni.Contains();
+            //denemeDegiskeni.CopyTo();
+            //denemeDegiskeni.EndsWith();
+            denemeDegiskeni.EnumerateRunes();
+            //denemeDegiskeni.Equals();
+            denemeDegiskeni.GetEnumerator();
+            denemeDegiskeni.GetHashCode();
+            denemeDegiskeni.GetType();
+            denemeDegiskeni.GetTypeCode();
+            //denemeDegiskeni.IndexOf();
+            //denemeDegiskeni.IndexOfAny();
+            //denemeDegiskeni.Insert();
+            denemeDegiskeni.IsNormalized();
+            //denemeDegiskeni.LastIndexOf();
+            //denemeDegiskeni.LastIndexOfAny();
+            //denemeDegiskeni.Length();
+            denemeDegiskeni.Normalize();
+            //denemeDegiskeni.PadLeft();
+            //denemeDegiskeni.PadRight();
+            //denemeDegiskeni.Remove();
+            //denemeDegiskeni.Replace();
+            denemeDegiskeni.Split();
+           // denemeDegiskeni.StartsWith();
+           //denemeDegiskeni.Substring();
+            denemeDegiskeni.ToCharArray();
+            denemeDegiskeni.ToLower();
+            denemeDegiskeni.ToLowerInvariant();
+            denemeDegiskeni.ToString();
+            denemeDegiskeni.ToUpper();
+            denemeDegiskeni.ToUpperInvariant();
+            denemeDegiskeni.Trim();
+            denemeDegiskeni.TrimEnd();
+            denemeDegiskeni.TrimStart();
+            
+
+
             //denemeDegiskeni.IndexOf
             //denemeDegiskeni.Split
             //denemeDegiskeni.Contains

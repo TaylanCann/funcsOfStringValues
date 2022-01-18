869904355cbeb73bf1f79ccdd6e1ac23c7123353
TaylanCann <taylancanh@gmail.com>
2022-01-18 17:03:23 +0300

    Add project files.

---
+++ funcsOfStringValues/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace funcsOfStringValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir kelime giriniz lütfen : ");
+
+            string denemeDegiskeni;
+            denemeDegiskeni = Console.ReadLine();
+            //denemeDegiskeni.IndexOf
+            //denemeDegiskeni.Split
+            //denemeDegiskeni.Contains
+            //denemeDegiskeni.Substring
+            //denemeDegiskeni.Substring
+
+            Console.WriteLine($"Girilen kelime : {denemeDegiskeni}");
+
+
+        }
+    }
+}
